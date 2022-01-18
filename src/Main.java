44907fe39de00a7a2a0fb06400ea49c34342ffd1
vlad2807 <vladus0101@gmail.com>
2022-01-18 18:00:26 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	new Main().run();
+
+
+
+    }
+
+    private void run() {
+       int[] mass = fillMass(20);
+        int max = mass[0];
+        for (int i = 1; i < mass.length; i++) {
+            if (mass [1]  > max) max = mass[i];
+
+            }
+        System.out.println(max);
+        }
+
+
+
+    private int[] fillMass(int n) {
+        int[] mass = new int[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            mass[i] =  random.nextInt(100);
+        }
+        return mass;
+    }
+
+
+}
+
